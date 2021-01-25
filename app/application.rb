5236557b3d7f586904a282560004301218688934
theo <teddyhoover@gmail.com>
2021-01-25 11:12:08 -0600

    commit tree 69b4a6cc7b1fc7a0d2e92b05a638c89a513a175c

---
+++ app/application.rb
@@ -0,0 +1,10 @@
+class Application
+    def call(env)
+        resp = Rack::Response.new
+        time = Time.new
+        
+            resp.write "Morning"
+            resp.write "Afternoon"
+        resp.finish
+    end
+end
